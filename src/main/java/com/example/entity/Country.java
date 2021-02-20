da0c3fd9dddb345b9e4a929ad3a6e6b983273fdb
Lucian Oancea <lucian.oancea@itsmartsystems.eu>
2021-02-20 17:54:48 +0200

    Added 2021022016630_changelog for create tables address,customer, orders and order _item and added to master.xml

---
+++ src/main/java/com/example/entity/Country.java
@@ -0,0 +1,27 @@
+package com.example.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import lombok.Data;
+
+import javax.persistence.*;
+import java.util.List;
+
+@Entity
+@Table(name = "country")
+@Data
+public class Country {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Integer id;
+
+    @Column(name = "code")
+    private String code;
+
+    @Column(name = "name")
+    private String name;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "country")
+    @JsonIgnore
+    private List<State> states;
+}
